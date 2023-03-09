8e10dc17f1f8397fe1626b6b6067ffaab3eaced2
ghost <--->
2023-03-09 19:15:10 +0300

    complete

---
+++ lab3/Calculator.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Windows.Controls;
+
+
+namespace lab3
+{
+    public class Calculator
+    {
+        private ParamStruct Params;
+
+        private BackgroundWorker Back { get; set; } = null;
+
+        private bool Success;
+        public bool GetSuccess() { return Success; }
+        private Exception LastExcept { get; set; } = null;
+        public Exception GetException() { return LastExcept; }
+
+        private ResultStruct[] Results;
+        public ResultStruct[] GetResults() { return Results; }
+
+        public Calculator(ParamStruct param)
+        {
+            if (param.XIterCount < 1)
+            {
+                Success = false;
+                LastExcept = new Exception("Некорректные значения.\nПересоздайте класс 'Calculator'");
+                return;
+            }
+            else Success = true;
+            Params = new ParamStruct { X0 = param.X0, XIterCount = param.XIterCount, Incr = param.Incr };
+            Results = new ResultStruct[param.XIterCount];
+        }
+        public Calculator(ParamStruct param, BackgroundWorker back)
+        {
+            if (param.XIterCount < 1)
+            {
+                Success = false;
+                LastExcept = new Exception("Некорректные значения.\nПересоздайте класс 'Calculator'");
+                return;
+            }
+            else Success = true;
+            Params = new ParamStruct { X0 = param.X0, XIterCount = param.XIterCount, Incr = param.Incr };
+            Results = new ResultStruct[param.XIterCount];
+            Back = back;
+        }
+
+
+        public ResultStruct[] Calculate()
+        {
+            if (!Success)
+            {
+                return null;
+            }
+
+            try
+            {
+                int i = 0;
+                int x = Params.X0;
+                do
+                {
+                    Results[i] = F(x);
+
+                    x += Params.Incr;
+                    i++;
+
+                    if (Back != null && i % 10e+0 == 0)
+                    {
+                        if (Back.CancellationPending)
+                        {
+                            return Results;
+                        }
+                        Back.ReportProgress((i * 100) / Results.Length, Results);
+                    }
+                }
+                while (i < Results.Length);
+            }
+            catch (Exception ex) { LastExcept = ex; return null; }
+
+            return Results;
+        }
+
+
+        private ResultStruct F(int x)
+        {
+            ResultStruct result = new ResultStruct()
+            {
+                X = x,
+                F1x = F1(x),
+                F2x = F2(x),
+                F3x = F3(x),
+                F4x = F4(x)
+            };
+            return result;
+        }
+
+        private double F1(int x)//exp(x/pi)
+        {
+            double res = 0;
+            try
+            {
+                res = Math.Exp(x / Math.PI);
+                res = Math.Round(res, 2);
+            }
+            catch (Exception ex)
+            {
+                if (LastExcept != null)
+                    LastExcept = ex;
+            }
+            if (double.IsNaN(res) || double.IsInfinity(res))
+                res = 0;
+            return res;
+        }
+        private double F2(int x)//log4(1-1/(1-x))
+        {
+            double res = 0;
+            try
+            {
+                res = Math.Log(1 - 1 / (1 - x), 4);
+                res = Math.Round(res, 2);
+            }
+            catch (Exception ex)
+            {
+                if (LastExcept != null)
+                    LastExcept = ex;
+            }
+            if (double.IsNaN(res) || double.IsInfinity(res))
+                res = 0;
+            return res;
+        }
+        private double F3(int x)
+        {
+            double res = 0;
+            try
+            {
+                if (x > 0)//sh(x^2-ln(x))
+                {
+                    double lx = Math.Pow(x, 2) - Math.Log(x, Math.E);
+                    res = Math.Exp(lx) - Math.Exp(-lx) / 2;
+                    res = Math.Round(res, 2);
+                }
+                else//tg(1/x^2)
+                {
+                    res = Math.Tan(1 / Math.Pow(x, 2));
+                    res = Math.Round(res, 2);
+                }
+            }
+            catch (Exception ex)
+            {
+                if (LastExcept != null)
+                    LastExcept = ex;
+            }
+            if (double.IsNaN(res) || double.IsInfinity(res))
+                res = 0;
+            return res;
+        }
+        private double F4(int x)
+        {
+            double res = 0;
+            try
+            {
+                double lRes = 0;
+                for (int i = 1; i < 10e+6; i++)//до 1 миллиона
+                {
+                    lRes += 1 / (x + Math.Pow(i, 1 / 2));
+                }
+                res = lRes;
+                res = Math.Round(res, 2);
+            }
+            catch (Exception ex)
+            {
+                if (LastExcept != null)
+                    LastExcept = ex;
+            }
+
+            if (double.IsNaN(res) || double.IsInfinity(res))
+                res = 0;
+            return res;
+        }
+
+    }
+
+
+    public struct ResultStruct
+    {
+        public int X;
+        public double F1x, F2x, F3x, F4x;
+        public double Fx() { return F1x + F2x + F3x + F4x; }
+    }
+    public struct ParamStruct
+    {
+        public int X0, XIterCount, Incr;
+    }
+
+}
---
+++ lab3/MainWindow.xaml.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+
+
+namespace lab3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        BackgroundWorker Back;
+        Calculator calculator;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Back = new BackgroundWorker();
+        }
+
+
+
+        private void BtnCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            if (Back.IsBusy)
+            {
+                if (MessageBox.Show("Прервать вычисление?", "Внимание!",
+                                   MessageBoxButton.YesNo,
+                                   MessageBoxImage.Question,
+                                   MessageBoxResult.Yes,
+                                   MessageBoxOptions.ServiceNotification) != MessageBoxResult.Yes)
+                {
+                    return;
+                }
+                else Back.CancelAsync();
+            }
+            else
+            {
+                ParamStruct param = new ParamStruct();
+                try
+                {
+                    param.X0 = Convert.ToInt32(TbX0.Text);
+                    param.XIterCount = Convert.ToInt32(TbIterCount.Text);
+                    param.Incr = Convert.ToInt32(TbIncr.Text);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    return;
+                }
+
+                Back = new BackgroundWorker
+                {
+                    WorkerReportsProgress = true,
+                    WorkerSupportsCancellation = true,
+                };
+                Back.DoWork += Back_DoWork;
+                Back.ProgressChanged += Back_ProgressChanged;
+                Back.RunWorkerCompleted += Back_RunWorkerCompleted;
+
+                calculator = new Calculator(param, Back);
+                if (!calculator.GetSuccess())
+                {
+                    MessageBox.Show(calculator.GetException().Message);
+                    return;
+                }
+
+                Back.RunWorkerAsync(param);
+                BtnCalculate.Content = "Прервать";
+                DGResult.HeadersVisibility = DataGridHeadersVisibility.All;
+
+            }
+
+        }
+
+
+
+        private void Back_DoWork(object sender, DoWorkEventArgs e)
+        {
+            try
+            {
+                if (!calculator.GetSuccess())
+                    return;
+                calculator.Calculate();
+
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+        }
+
+
+        private void Back_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            FillTable((ResultStruct[])e.UserState);
+        }
+
+        private void Back_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            FillTable(calculator.GetResults());
+            BtnCalculate.Content = "Рассчитать";
+
+            Exception ex = calculator.GetException();
+            if (ex != null)
+                MessageBox.Show(ex.Message);
+        }
+
+
+        private void FillTable(ResultStruct[] results)
+        {
+            if (calculator == null)
+                return;
+
+            var t = results.Select(x => new
+            {
+                X = x.X,
+                F1 = x.F1x,
+                F2 = x.F2x,
+                F3 = x.F3x,
+                F4 = x.F4x,
+                F = x.Fx()
+            });
+
+            DGResult.ItemsSource = t;
+
+            //для переименования колонок
+            //DGResult.Columns[0].Header = "Название кадра";
+            //DGResult.Columns[1].Header = "Количество кадров";
+            //DGResult.Columns[2].Header = "Ошибок нумерации";
+            //DGResult.Columns[3].Header = "Ошибок CRC";
+        }
+    }
+}
